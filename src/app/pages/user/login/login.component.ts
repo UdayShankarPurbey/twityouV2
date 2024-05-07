fc817c2cb197b4501c536dece7d82ee8b0406101
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-05-07 12:53:38 +0530

    add toster and error msg service

--- src/app/pages/user/login/login.component.ts
+++ src/app/pages/user/login/login.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule,Router } from '@angular/router';
+import { ErrorMsgService } from '../../../services/error-msg.service';
+import { ToasterService } from '../../../services/toaster.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +11,20 @@ import { RouterLink } from '@angular/router';
   imports: [
     RouterLink,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginForm : FormGroup
-  constructor(private fb : FormBuilder , private http : HttpClient) {
+  constructor(private fb : FormBuilder ,
+    private http : HttpClient ,
+    private router : Router,
+    private errormsg : ErrorMsgService,
+    private message : ToasterService,
+  ) {
     this.loginForm = this.fb.group({
       email: [''],
       username: [''],
@@ -28,11 +36,25 @@ export class LoginComponent {
     console.log(this.loginForm.value);
     this.http.post('http://localhost:8000/api/v1/users/login',this.loginForm.value).subscribe((res : any) => {
       console.log(res)
-      localStorage.setItem('accessToken',res.message.accessToken)
-      localStorage.setItem('refreshToken',res.message.refreshToken)
-      //todo : redirection of page
-    })
+      localStorage.setItem('accessToken',res?.message?.accessToken)
+      localStorage.setItem('refreshToken',res?.message?.refreshToken)
+      if(res?.message?.user) {
+        this.router.navigate(['/youtube'])
+      } else {
+        this.message.showToast('Login First' , "warning")
+        this.router.navigate(['/login']);
+      }
+
+    },
+    (error) => {
+      console.log(error.error);
+      this.message.showToast(this.errormsg.errorMsgDisp(error.error) , 'question')   
+    }
+  )
+
 
   }
+
+  
 
 }
