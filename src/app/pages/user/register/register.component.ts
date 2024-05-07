bc73427f07481a99f8a6e3ec18e705a9b32681ba
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-05-07 12:53:54 +0530

    chages in register component

--- src/app/pages/user/register/register.component.ts
+++ src/app/pages/user/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -7,14 +8,18 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [
     RouterLink,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   registerForm : FormGroup;
-  constructor(private fb : FormBuilder) { 
+  constructor(
+    private fb : FormBuilder ,
+    private http : HttpClient
+  ) { 
     this.registerForm = fb.group({
       password:[],
       fullName:[],
@@ -22,12 +27,33 @@ export class RegisterComponent {
       username:[],
       mobNo:[],
       avatarImage : [],
+      coverImage : [],
     })
   }
 
   userRegister() {
     console.log(this.registerForm.value);
+    this.http.post('http://localhost:8000/api/v1/users/register', this.registerForm.value).subscribe((res : any) => console.log(res) )
+
   }
+//   this.http.post('http://localhost:8000/api/v1/users/login',this.loginForm.value).subscribe((res : any) => {
+//     console.log(res)
+//     localStorage.setItem('accessToken',res.message.accessToken)
+//     localStorage.setItem('refreshToken',res.message.refreshToken)
+//     if(res?.message?.user) {
+//       this.router.navigate(['/youtube'])
+//     } else {
+//       this.message.showToast('Login First' , "warning")
+//       this.router.navigate(['/login']);
+//     }
+
+//   },
+//   (error) => {
+//     console.log(error.error);
+//     this.message.showToast(this.errormsg.errorMsgDisp(error.error) , 'question')   
+//   }
+// )
+
 
 
 }
