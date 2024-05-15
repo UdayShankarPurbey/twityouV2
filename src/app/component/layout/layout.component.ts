8df6928b9e058ea228d40ac10fd6b65a4debae64
udayshankar754 <uday.naxtre@gmail.com>
2024-05-15 16:37:23 +0530

    changes in menu in layout

--- src/app/component/layout/layout.component.ts
+++ src/app/component/layout/layout.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { GetTokenService } from '../../services/get-token.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    CommonModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export class LayoutComponent  implements OnInit {
+  userData : any;
+  modalVisibility : boolean = false;
+  constructor(private token : GetTokenService) {}
+  ngOnInit(): void {
+    this.userData = this.token.getLoggedInUserData()
+    console.log(this.userData);
+  }
 
+  OpenMenu() {
+    this.modalVisibility = !this.modalVisibility;
+  }
 }
