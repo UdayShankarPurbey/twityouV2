13ab7492c7e7475e01c8195aebfea0e4739843e5
udayshankar754 <uday.naxtre@gmail.com>
2024-05-23 11:31:40 +0530

    added replied and edit replied service

--- src/app/services/comment/comment.service.ts
+++ src/app/services/comment/comment.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { end_points } from '../../core/endpoints';
-import { GetTokenService } from '../get-token.service';
+import { GetTokenService } from '../getToken/get-token.service';
 import { TypeEnum } from '../../core/type-enum';
 
 @Injectable({
@@ -36,11 +36,20 @@ export class CommentService {
     return this.http.post(`${environment.api_base_url+end_points.comment}/${type}/${id}`, reqobj, { headers: this.token.getToken() });
   }
 
-  addRepliedComment(id :string, content : any) {
-    let reqobj = {
-      content : content
-    }
-    return this.http.patch(`${environment.api_base_url+end_points.commentReplied}/${id}`, reqobj, { headers: this.token.getToken() });
+  addRepliedComment(id :string, data : any) {
+    return this.http.post(`${environment.api_base_url+end_points.commentReplied}/${id}`, {  
+      content : data
+    }, { headers: this.token.getToken() });
+  }
+
+  editRepliedComment(commentId :string,repliedCommentId : string, data : any) {
+    return this.http.patch(`${environment.api_base_url+end_points.commentReplied}/${commentId}/${repliedCommentId}`, {  
+      content : data
+    }, { headers: this.token.getToken() });
+  }
+
+  deleteRepliedComment(commentId :string,repliedCommentId : string) {
+    return this.http.delete(`${environment.api_base_url+end_points.commentReplied}/${commentId}/${repliedCommentId}`, { headers: this.token.getToken() });
   }
 
 }
