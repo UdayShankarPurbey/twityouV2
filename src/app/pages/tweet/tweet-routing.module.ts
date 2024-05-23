0b614f6b35cf907b8867acd8333d97372f1561c7
udayshankar754 <uday.naxtre@gmail.com>
2024-05-23 12:55:46 +0530

    remove  analytics and notification pages

--- src/app/pages/tweet/tweet-routing.module.ts
+++ src/app/pages/tweet/tweet-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TweetDashboardComponent } from './tweet-dashboard/tweet-dashboard.component';
-import { NotificationComponent } from './notification/notification.component';
-import { AnalyticsComponent } from './analytics/analytics.component';
 
 const routes: Routes = [
   { path : '', redirectTo : 'dashboard', pathMatch : 'full'},
   {path: 'dashboard', component: TweetDashboardComponent, children : 
     [
       {path: 'home', component: HomeComponent},
-      {path: 'notifications', component: NotificationComponent},
-      {path : 'analytics', component: AnalyticsComponent},
     ]
   },
  
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    api_base_url: "http://localhost:8000/api/v1",
+    // api_base_url: "http://localhost:8000/api/v1",
+    api_base_url: "http://localhost:4200/api/v1",
+
 };
